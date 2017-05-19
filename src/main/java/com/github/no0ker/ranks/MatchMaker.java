b8bdea0854707324b26d503b721b96d1935bf8a1
rshavaliev <rshavaliev@diasoft.ru>
2017-05-19 13:45:16 +0300

    init commit

---
+++ src/main/java/com/github/no0ker/ranks/MatchMaker.java
@@ -0,0 +1,12 @@
+package com.github.no0ker.ranks;
+
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+
+public class MatchMaker {
+    public String getResult(File f){
+        return StringUtils.EMPTY;
+    }
+}
---
+++ src/test/java/com/github/no0ker/ranks/MatchMakerTest.java
@@ -0,0 +1,10 @@
+package com.github.no0ker.ranks;
+
+import org.junit.Test;
+
+public class MatchMakerTest {
+    @Test
+    public void test01(){
+        System.out.println(new MatchMaker().getResult(null));
+    }
+}
